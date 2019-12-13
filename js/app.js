45b9925f2c789629caa2f6f393170af12485286f
Ellie <cassona08@gmail.com>
2019-12-13 15:47:06 +0000

    adds mvp

--- js/app.js
+++ js/app.js
@@ -1,3 +1,52 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JS loaded')
-})
+
+  const newRecipe = document.querySelector('#addRecipe');
+  newRecipe.addEventListener('submit', handleNewRecipeFormSubmit)
+
+  const deleteAllButton = document.querySelector('#deleteAll');
+deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
+  })
+
+  const handleNewRecipeFormSubmit = function (event) {
+    event.preventDefault();
+      console.log('add new item')
+
+
+      const recipeListItem = createRecipeListItem(event.target);
+      const recipeList = document.querySelector('ul');
+      recipeList.appendChild(recipeListItem);
+
+
+        event.target.reset();
+  };
+
+
+
+  const createRecipeListItem = function (form) {
+    const recipeListItem = document.createElement('li');
+    recipeListItem.classList.add = '#recipeElement'
+
+    const name = document.createElement('h3');
+    name.textContent = form.name.value;
+    recipeListItem.appendChild(name);
+
+    const cuisine = document.createElement('h4');
+    cuisine.textContent = form.cuisine.value;
+    recipeListItem.appendChild(cuisine);
+
+    const url = document.createElement('a');
+    url.href = form.url.value;
+    url.target = "_blank"
+    url.textContent = `Link to Recipe`;
+    recipeListItem.appendChild(url);
+
+    return recipeListItem;
+  }
+
+
+  const handleDeleteAllClick = function (event) {
+    const recipeList = document.querySelector('#recipeList');
+    recipeList.innerHTML = '';
+  }
